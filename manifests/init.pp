e1fb9e678c3741c186dbee6c825389179122c305
rajendra <rajendra.peddada@infostretch.com>
2016-11-15 11:56:58 -0800

    Update init.pp
    adding mlocate

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,5 +1,5 @@
 class packages {
-        $packages = ["java-1.7.0-openjdk-devel", "curl", "wget", "vim-enhanced.x86_64", "git", "nodejs", "npm", "cronie"]
+        $packages = ["java-1.7.0-openjdk-devel", "curl", "wget", "mlocate", "vim-enhanced.x86_64", "git", "nodejs", "npm", "cronie"]
         package { $packages:
                 ensure => present,
         }
