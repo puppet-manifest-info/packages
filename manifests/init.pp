5786279741fb9bc0bd425527fbfd368242fd1126
rajendra <rajendra.peddada@infostretch.com>
2016-12-23 10:44:56 -0800

    Update init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,6 +1,6 @@
 class packages {
         include services
-        $packages = ["java-1.8.0-openjdk.x86_64", "curl", "sudo", "system-config-services", "wget", "mlocate", "vim-enhanced.x86_64", "git", "nodejs", "npm", "cronie"]
+        $packages = ["java-1.8.0-openjdk.x86_64", "curl", "sudo", "unzip", "system-config-services", "wget", "mlocate", "vim-enhanced.x86_64", "git", "nodejs", "npm", "cronie"]
         package { $packages:
                 ensure => present,
         }
