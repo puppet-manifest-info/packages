e48f853aef44c763e7edb8eeb515c719b58e9fa8
rajendra <rajendra.peddada@infostretch.com>
2017-01-02 23:22:40 -0800

    Update init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,5 +1,5 @@
 class packages {
-        $packages = ["java-1.8.0-openjdk.x86_64", "curl", "sudo", "unzip", "system-config-services", "wget", "mlocate", "vim-enhanced.x86_64", "git", "nodejs", "npm", "cronie"]
+        $packages = ["epel-release", "java-1.8.0-openjdk.x86_64", "curl", "sudo", "unzip", "system-config-services", "wget", "mlocate", "vim-enhanced.x86_64", "git", "nodejs", "npm", "cronie"]
         package { $packages:
                 ensure => present,
         }
