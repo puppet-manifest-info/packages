ae36d82263db810712a8eb8994c84213de6e7795
rajendra <rajendra.peddada@infostretch.com>
2017-01-02 23:17:35 -0800

    Delete services.pp

--- manifests/services.pp
+++
@@ -1,8 +0,0 @@
-class packages::services {
-        $services = ["openssh-server", "openssh-clients"]
-        services { $services:
-                ensure => present,
-                ensure => running,
-        }
-
-}
